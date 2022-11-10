efaf39ec064ffc70e3da84333c04eadda531b614
yarahossamboo <yarahossam222@gmail.com>
2022-11-10 21:19:11 +0200

    Converting state to array done credits to veroooooooo

--- Converter.py
+++ Converter.py
@@ -1,34 +1,35 @@
-
 class Converter:
+
     def convertStateToArray(self, state):
         rows, cols = (6, 7)
-        arrayState = [['white' for i in range(cols)] for j in range(rows)]
+        arrayState = [['w' for i in range(cols)] for j in range(rows)]
         state = str(state)
-        index = 0
+        index = len(state)
         col = 0
         while col != 7:
-            print(state[index: index+3])
-            emptyIndex = int(state[index: index+3], 2)  # convert binary rep of empty space to decimal
+            nextIndex = int(state[index - 3: index], 2)  # convert binary rep of empty space to decimal
+            i = 0
+            startIndex = index - 4
+            while nextIndex != 0:
+                if state[startIndex] == '0':
+                    arrayState[i][col] = 'y'
+                else:
+                    arrayState[i][col] = 'r'
+                startIndex = startIndex - 1
+                i = i + 1
+                nextIndex = nextIndex - 1
 
-            if emptyIndex != 0:
-                for i in range(0, emptyIndex - 1):
-                    print("d5alt")
-                    if state[index + i] == '0':
-                        arrayState[i][col] = 'yellow'
-                    else:
-                        arrayState[i][col] = 'red'
-            index = index + emptyIndex + 2
+            index = index - 9
             col = col + 1
-            for row in arrayState:
-                print(row)
+
         return arrayState
 
 
 if __name__ == '__main__':
     conv = Converter()
-    arr = conv.convertStateToArray(101110001010000100100001000000)
-
-
+    arr = conv.convertStateToArray(111111000111111001111100011110111100111111001111000100111100010)
+    for row in arr:
+        print(row)
     '''
     def convertArrayToStarte(arr):
         representation = ""
@@ -62,9 +63,3 @@ if __name__ == '__main__':
         return representation
 
     '''
-
-
-
-
-
-
