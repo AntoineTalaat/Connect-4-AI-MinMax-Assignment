fe60c2b023ae6691e364111fff1d1a90cccbf729
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-11-10 20:16:34 +0200

    :))

--- Converter.py
+++ Converter.py
@@ -1,5 +1,47 @@
-import State
+import numpy as np
 class Converter:
-    def convertStateToArray(self, Int state):
-        arrayState = [6][7]
-        state = state
+    '''
+    def convertArrayToStarte(arr):
+        representation = ""
+        rows = len(arr)
+        columns = len(arr[0])
+        for j in range (columns):
+           index = 7
+           for i in range (rows):
+               print("i ",i," j ",j)
+               if arr[i][j] == 'w':
+
+                   index = i
+                   print("find white at ",index)
+                   break
+
+           print("start of spaces >> ",index)
+           binary = "{0:03b}".format(index)
+           print("Binary representaion ",binary)
+           representation +=binary
+           print("Board representaion ",representation)
+
+           for x in range(index):
+               if arr[x][j] == 'r':
+                   representation+="1"
+                   print(representation)
+               elif arr[x][j] == 'y':
+                   representation+="0"
+                   print(representation)
+
+        representation = int(representation)
+        return representation
+
+    '''
+
+
+
+
+
+    if __name__ == '__main__':
+     
+
+
+
+
+
