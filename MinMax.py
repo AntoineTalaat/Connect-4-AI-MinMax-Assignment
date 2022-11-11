0c5c292f39f8b813f67634f2786c64946eb28957
yarahossamboo <yarahossam222@gmail.com>
2022-11-11 03:19:02 +0200

    minmax without alpha beta done but not tested

--- MinMax.py
+++ MinMax.py
@@ -1,16 +1,40 @@
 import State
+
+
 class MinMax:
 
-    def buildKLevels(self,k,currentState):
-        if(k!=0):
+    def buildKLevels(self, k, currentState):
+        if k != 0:
             currentState.findMyChildren()
-            k=k-1
-            for i in range (len(currentState.children)):
+            k = k - 1
+            for i in range(len(currentState.children)):
                 self.buildKLevels(k, currentState.children[i])
 
+    def maxValue(self, state):
+        v = float('-inf')
+        for child in state.children:
+            value = self.MinMax(k - 1, child, withAlphaBeta)
+            if value > v:
+                v = value
+                move = child
+        return v, move
 
+    def minValue(self, state):
+        v = float('inf')
+        for child in state.children:
+            value = self.MinMax(k - 1, child, withAlphaBeta)
+            if value < v:
+                v = value
+                move = child
+        return v, move
 
-    def MinMax (self,k , currentState , withAlphaBeta):
-        self.buildKLevels(k,currentState)
+    def MinMax(self, k, currentState, withAlphaBeta):
+        self.buildKLevels(k, currentState)
+        if k == 0:
+            return currentState.getScore()
+        if currentState.max:
+            v, move = minValue(currentState)
+        else:
+            v, move = maxValue(currentState)
 
-
+        return move
--- State.py
+++ State.py
@@ -1,16 +1,25 @@
+import Converter
+
+
 class State:
     rep = int('0b111111000111111000111111000111111000111111000111111000111111000', 2)
-    children  = []
+    children = []
     max = True
+
     # alpha = int('-inf')
     # beta = int ('int')
 
+    def getScore(self):
+        if self.isFull():
+            return 0#score mn mark
+        else:
+            return 0#heuristic mn tony
 
     def changeBit(parent, pos, bit):
         mask = 1 << pos
         parent = (parent & ~mask) | ((bit << pos) & mask)
 
-    def addToColumn(self,parent, col, playerColour):
+    def addToColumn(self, parent, col, playerColour):
         chunckShift = col * 9
         temp = 7 << chunckShift
         indexPlace = parent & temp
@@ -18,31 +27,38 @@ class State:
 
         # Modify the bit itself
         if playerColour == 'y':
-            self.changeBit(parent,position, 0)
+            self.changeBit(parent, position, 0)
         elif playerColour == 'r':
-            self.changeBit(parent,position, 1)
+            self.changeBit(parent, position, 1)
 
         # modify the index in representation
         binary = "{0:03b}".format(indexPlace + 1)  # 101
         for i in range(len(binary)):
-            self.changeBit(parent,chunckShift + i, int(binary[2 - i]))
+            self.changeBit(parent, chunckShift + i, int(binary[2 - i]))
 
         return parent
 
     def findMyChildren(self):
         myChildMax = not self.max
-        for i in range (7):
+        for i in range(7):
             child = State()
-            if(self.max == True):
-                child.rep = self.addToColumn(self.rep,i,'y')
+            if (self.max == True):
+                child.rep = self.addToColumn(self.rep, i, 'y')
                 child.max = myChildMax
-            else :
+            else:
                 child.rep = self.addToColumn(self.rep, i, 'r')
                 child.max = myChildMax
 
             self.children.append(child)
 
-
+    def isFull(self):
+        converter = Converter.Converter()
+        arrayState = converter.convertStateToArray(self.rep)
+        for row in arrayState:
+            for i in range(len(row)):
+                if arrayState[row][i] == 'w':
+                    return False
+        return True
 
 
 if __name__ == '__main__':
