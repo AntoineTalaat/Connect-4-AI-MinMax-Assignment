4aec53332eea785baae6bdb744843a373f3f24e6
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-11-11 02:24:41 +0200

    find k levels children
    MinMax file edited
    State file

--- MinMax.py
+++ MinMax.py
@@ -1,1 +1,16 @@
+import State
 class MinMax:
+
+    def buildKLevels(self,k,currentState):
+        if(k!=0):
+            currentState.findMyChildren()
+            k=k-1
+            for i in range (len(currentState.children)):
+                self.buildKLevels(k, currentState.children[i])
+
+
+
+    def MinMax (self,k , currentState , withAlphaBeta):
+        self.buildKLevels(k,currentState)
+
+
--- State.py
+++ State.py
@@ -1,29 +1,48 @@
 class State:
     rep = int('0b111111000111111000111111000111111000111111000111111000111111000', 2)
-
+    children  = []
+    max = True
     # alpha = int('-inf')
     # beta = int ('int')
 
-    def changeBit(self, pos, bit):
-        mask = 1 << pos
-        self.rep = (self.rep & ~mask) | ((bit << pos) & mask)
 
-    def addToColumn(self, col, playerColour):
+    def changeBit(parent, pos, bit):
+        mask = 1 << pos
+        parent = (parent & ~mask) | ((bit << pos) & mask)
+
+    def addToColumn(self,parent, col, playerColour):
         chunckShift = col * 9
         temp = 7 << chunckShift
-        indexPlace = self.rep & temp
+        indexPlace = parent & temp
         position = chunckShift + indexPlace + 3
 
         # Modify the bit itself
         if playerColour == 'y':
-            self.changeBit(position, 0)
+            self.changeBit(parent,position, 0)
         elif playerColour == 'r':
-            self.changeBit(position, 1)
+            self.changeBit(parent,position, 1)
 
         # modify the index in representation
         binary = "{0:03b}".format(indexPlace + 1)  # 101
         for i in range(len(binary)):
-            self.changeBit(chunckShift + i, int(binary[2 - i]))
+            self.changeBit(parent,chunckShift + i, int(binary[2 - i]))
+
+        return parent
+
+    def findMyChildren(self):
+        myChildMax = not self.max
+        for i in range (7):
+            child = State()
+            if(self.max == True):
+                child.rep = self.addToColumn(self.rep,i,'y')
+                child.max = myChildMax
+            else :
+                child.rep = self.addToColumn(self.rep, i, 'r')
+                child.max = myChildMax
+
+            self.children.append(child)
+
+
 
 
 if __name__ == '__main__':
